57180154f29f5c446a7f4a59a0aedd45fb2c868d
Jeff Buchbinder <jeff@ourexchange.net>
2012-11-21 14:59:55 -0500

    Documentation, split source a bit more logically.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-DATAGEN
-=======
+# DATAGEN
 
 FreeMED randomized patient medical data generator.
+
+## DATABASES
+
+Databases used by this medical data generator include:
+
+* [US Census Database](http://www.census.gov/genealogy/www/data/1990surnames) - Provides a list of popular first and last names.
+* [The Zip Code Database Project](http://zips.sourceforge.net/) - Provides city, state, postal code, and lat/lon information.
 
--- random.go
+++ random.go
@@ -19,54 +19,6 @@ func GenerateRandomDate() time.Time {
 	return time.Date(year, month, day, 12, 0, 0, 0, time.UTC)
 }
 
-type Gender int
-
-const (
-	_ = 0
-	Male
-	Female
-)
-
-type NameGenerator struct {
-	LastNames   []string
-	FFirstNames []string
-	MFirstNames []string
-}
-
-func NewNameGenerator() *NameGenerator {
-	n := &NameGenerator{}
-	n.Init()
-	return n
-}
-
-// Load data for name generator from files
-func (self *NameGenerator) Init() {
-	var e error
-	self.LastNames, e = loadArrayFromFile("data/names/dist.all.last.txt.json")
-	if e != nil {
-		panic(e.Error())
-	}
-	self.FFirstNames, e = loadArrayFromFile("data/names/dist.female.first.txt.json")
-	if e != nil {
-		panic(e.Error())
-	}
-	self.MFirstNames, e = loadArrayFromFile("data/names/dist.male.first.txt.json")
-	if e != nil {
-		panic(e.Error())
-	}
-}
-
-// Generate a randomized name.
-func (self *NameGenerator) Generate(s Gender) (first string, last string) {
-	last = self.LastNames[rand.Intn(len(self.LastNames))]
-	if s == Male {
-		first = self.MFirstNames[rand.Intn(len(self.MFirstNames))]
-	} else {
-		first = self.FFirstNames[rand.Intn(len(self.FFirstNames))]
-	}
-	return
-}
-
 // Deserialize a JSON array from a file
 func loadArrayFromFile(f string) ([]string, error) {
 	fi, fierr := os.Stat(f)
